73f0a1e66f3fba09cb64c8076a7c25b0d6150708
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2024-04-06 16:14:47 -0300

    Atualizar o MemoryManager.java

--- src/so/memory/MemoryManager.java
+++ src/so/memory/MemoryManager.java
@@ -1,10 +1,6 @@
 package so.memory;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import so.SubProcess;
