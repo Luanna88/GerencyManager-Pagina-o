cc8d3380badc0bde018e8b2070f99afb5ee928b1
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2024-04-12 01:32:24 -0300

    Update Execute.java

--- src/so/Execute.java
+++ src/so/Execute.java
@@ -4,6 +4,8 @@ public class Execute {
 	
 	public static void main (String[]args) {
 		
+		while (!SystemOperation.scheduler.isEmpty()) {
+			
 		Process p1 = (Process)SystemOperation.systemCall(SystemCallType.CREATE_PROCESS,50);
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p1);
 		
@@ -13,10 +15,13 @@ public class Execute {
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p2);
 		
 		System.out.println("\n****************************************************************************************************************\n");
-
 		
 		Process p3 = (Process)SystemOperation.systemCall(SystemCallType.CREATE_PROCESS,28);
 		SystemOperation.systemCall(SystemCallType.WRITE_PROCESS, p3);
+		
+		System.out.println("\n****************************************************************************************************************\n");
+		
+		}
 		
 	}
 
